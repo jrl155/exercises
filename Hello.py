10002f58a049f309b4246ef10d3b3bcc588957df
jrl155 <jrl155la@hotmail.com>
2013-09-05 11:56:33 -0700

    Create Hello.py

---
+++ Hello.py
@@ -0,0 +1,23 @@
+print("Hello")
+
+input()
+
+print("Whats your name?")
+
+name = input()
+
+print("Hello", name)
+
+print("How was your day?")
+
+Day = input()
+
+print("Thats", Day)
+
+print("Whats your age?")
+
+input()
+
+print("The End")
+
+input("\nPress enter to exit")
